db9043492fedc9ec3321fd2ed4e09e25e7e780bd
Michael <laikahkeen@gmail.com>
2023-03-25 17:13:53 +0800

    setting up part 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import {FiSettings} from 'react-icons/fi'
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import './App.css'
+import "./App.css";
 
-const App = () => 
-<div>
-  <BrowserRouter>
-  <div className='flex relative dark:bg-main-dark-bg'>
-    <div className='fixed right-4 bottom-4 style' style={{zIndex:'1000'}}>
-      <TooltipComponent content='Settings' position='Top'>
+const App = () => (
+	<div>
+		<BrowserRouter>
+			<div className="flex relative dark:bg-main-dark-bg">
+				<div
+					className="fixed right-4 bottom-4 style"
+					style={{ zIndex: "1000" }}>
+					<TooltipComponent content="Settings" position="Top">
+						<button type="button" className="bg-blue-900 rounded-full text-3xl p-3 hover:drop-shadow-xl hover:bg-blue-500 text-white">
+							<FiSettings />
+						</button>
+					</TooltipComponent>
+				</div>
+			</div>
+		</BrowserRouter>
+	</div>
+);
 
-      </TooltipComponent>
-    </div>
-    </div>
-    </BrowserRouter>
-</div>
-
-export default App
+export default App;
